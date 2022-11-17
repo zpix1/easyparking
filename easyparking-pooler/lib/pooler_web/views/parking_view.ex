ab62c3cac4575448194010ecb1fc17bef4c8c041
Matvey Karpov <matveichk@gmail.com>
2022-11-17 21:47:22 +0700

    Поправил parking view

--- easyparking-pooler/lib/pooler_web/views/parking_view.ex
+++ easyparking-pooler/lib/pooler_web/views/parking_view.ex
@@ -27,7 +27,7 @@ defmodule PoolerWeb.ParkingView do
       "longitude" => longitude,
       "image_url" => parking.image_url,
       "processed_image_url" => parking.processed_image_url,
-      "distance_from_user" => parking[:distance_from_user]
+      "distance_from_user" => get_in(parking, [Access.key(:distance_from_user)])
     }
   end
 end
