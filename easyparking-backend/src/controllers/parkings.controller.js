e1619bbb0296753b3b05a4472e1fc519b47f6486
CalvinLezes <d.ermilov@g.nsu.ru>
2022-11-24 22:20:18 +0700

    format parkings.controller

--- easyparking-backend/src/controllers/parkings.controller.js
+++ easyparking-backend/src/controllers/parkings.controller.js
@@ -64,48 +64,51 @@ export function removeFavoriteParking(req, res) {
 }
 
 export function getParkingById(req, res) {
-  axios.get(`http://localhost:4000/api/v1/parkings-by-ids?ids[]=${req.params.parkingId}`)
-  .then(resp =>{
-    resp.data.map((parking)=>{
-      return res.status(200).json({
-        id: parking.id,
-        address: parking.address,
-        image_url: parking.image_url,
-        processed_image_url: parking.processed_image_url, 
-        latitude: parking.latitude, 
-        longitude: parking.longitude, 
-        title: parking.title
+  axios
+    .get(`http://localhost:4000/api/v1/parkings-by-ids?ids[]=${req.params.parkingId}`)
+    .then((resp) => {
+      resp.data.map((parking) => {
+        return res.status(200).json({
+          id: parking.id,
+          address: parking.address,
+          image_url: parking.image_url,
+          processed_image_url: parking.processed_image_url,
+          latitude: parking.latitude,
+          longitude: parking.longitude,
+          title: parking.title,
+        });
       });
+      return;
     })
-    return;
-  })
-  .catch((err)=>{
-    res.status(500).send({
-      message: err,
+    .catch((err) => {
+      res.status(500).send({
+        message: err,
+      });
+      return;
     });
-    return;
-  })
-  
 }
 
-export function getParkings(req, res){
+export function getParkings(req, res) {
   const user_latitude = req.body.latitude || 33.5854;
   const user_longitude = req.body.longitude || -15.3333;
   const offset = parseInt(req.body.offset) || 0;
   const count = parseInt(req.body.count) || 10;
-  const page = parseInt(offset/count) + 1;
-  const page_size = (offset%count) + count;
-  axios.get(`http://localhost:4000/api/v1/parking?user_latitude=${user_latitude}&user_longitude=${user_longitude}&page=${page}&page_size=${page_size}`)
-  .then(resp => {
-    const entries_paginated = resp.data.entries.slice(offset%count);
-    return res.status(200).json({
-      entries: entries_paginated
+  const page = parseInt(offset / count) + 1;
+  const page_size = (offset % count) + count;
+  axios
+    .get(
+      `http://localhost:4000/api/v1/parking?user_latitude=${user_latitude}&user_longitude=${user_longitude}&page=${page}&page_size=${page_size}`,
+    )
+    .then((resp) => {
+      const entries_paginated = resp.data.entries.slice(offset % count);
+      return res.status(200).json({
+        entries: entries_paginated,
+      });
     })
-  })
-  .catch((err)=>{
-    res.status(500).send({
-      message: err,
+    .catch((err) => {
+      res.status(500).send({
+        message: err,
+      });
+      return;
     });
-    return;
-  })
 }
